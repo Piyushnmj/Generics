0047d10140f9854b49bcdadf5b074b7b44adb4f7
Piyushnmj <piyushnmj@yahoo.com>
2022-11-07 17:02:32 +0530

    Find Max Integer Value

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -1,39 +1,13 @@
 ﻿namespace Generics
 {
-    public class PrintArray<T>
-    {
-        public T[] inputArray;
-
-        public PrintArray(T[] inputArray)
-        {
-            this.inputArray = inputArray;
-        }
-
-        public void toPrint()
-        {
-            Console.WriteLine($"\n{typeof(T)} Array: ");
-            foreach (var item in inputArray)
-            {
-                Console.WriteLine(item);
-            }
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generics Problems");
 
-            int[] intArray = { 1, 2, 3, 4, 5 };
-            double[] doubleArray = { 1.1, 1.2, 1.3, 1.4, 1.5 };
-            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
-
-            new PrintArray<int>(intArray).toPrint();
-            new PrintArray<double>(doubleArray).toPrint();
-            new PrintArray<char>(charArray).toPrint();
-
-            Console.ReadLine();
+            int output = MaxNumberCheck.MaxIntegerNumber(11, 111, 1111);
+            Console.WriteLine("\nMax integer number is: " + output);
         }
     }
 }
